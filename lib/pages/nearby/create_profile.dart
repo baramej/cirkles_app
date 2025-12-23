9b6950e6b3b26530350c54a6364b4a869aec8138
Syed Ali Raza <isyedaliraza@gmail.com>
2025-12-23 20:09:41 +0000

    feat: Add confirmation dialog for creating a nearby profile

--- lib/pages/nearby/create_profile.dart
+++ lib/pages/nearby/create_profile.dart
@@ -30,40 +30,69 @@ class CreateProfileController extends State<CreateProfile> {
   bool loading = false;
   String? error;
 
+  Future<void> createProfile() async {
+    final l10n = L10n.of(context);
+
+    setState(() {
+      loading = true;
+      error = null;
+    });
+
+    try {
+      final mxId = Matrix.of(context).client.userID!;
+      final userId = Supabase.instance.client.auth.currentUser!.id;
+      final locationString = 'POINT(${widget.long} ${widget.lat})';
+
+      await Supabase.instance.client.from('drivers').upsert({
+        'id': userId,
+        'car_make': makeController.text,
+        'car_model': modelController.text,
+        'car_year': yearController.text,
+        'car_plate': plateController.text,
+        'car_color': colorController.text,
+        'is_sharing': true,
+        'location': locationString,
+        'mx_id': mxId,
+      });
+
+      context.go('/main/nearby');
+    } catch (e) {
+      debugPrint('Error creating profile: $e');
+      setState(() {
+        loading = false;
+        error = l10n.nearbyCreateProfileError;
+      });
+    }
+  }
+
   Future<void> submit() async {
     if (formKey.currentState!.validate() == true) {
       final l10n = L10n.of(context);
 
-      setState(() {
-        loading = true;
-        error = null;
-      });
-
-      try {
-        final mxId = Matrix.of(context).client.userID!;
-        final userId = Supabase.instance.client.auth.currentUser!.id;
-        final locationString = 'POINT(${widget.long} ${widget.lat})';
-
-        await Supabase.instance.client.from('drivers').upsert({
-          'id': userId,
-          'car_make': makeController.text,
-          'car_model': modelController.text,
-          'car_year': yearController.text,
-          'car_plate': plateController.text,
-          'car_color': colorController.text,
-          'is_sharing': true,
-          'location': locationString,
-          'mx_id': mxId,
-        });
-
-        context.go('/main/nearby');
-      } catch (e) {
-        debugPrint('Error creating profile: $e');
-        setState(() {
-          loading = false;
-          error = l10n.nearbyCreateProfileError;
-        });
-      }
+      showDialog<bool>(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(l10n.nearbyCreateProfileConfirmDialogTitle),
+            content: Text(l10n.nearbyCreateProfileConfirmDialogBody),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  createProfile();
+                },
+                child: Text(l10n.nearbyCreateProfileConfirmDialogYes),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(l10n.nearbyCreateProfileConfirmDialogNo),
+              ),
+            ],
+          );
+        },
+      );
     }
   }
 
