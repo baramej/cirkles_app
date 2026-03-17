c5db9e66212a8827bcb4e0b4c3b5871010c05f9d
Syed Ali Raza <isyedaliraza@gmail.com>
2026-03-17 10:17:38 +0000

    Add secrets to gitignore

--- lib/config/secrets.dart
+++
@@ -1,9 +0,0 @@
-class Secrets {
-  static const kSupabaseUserPasswordSecret = "lDAgFv9ANcmPpra9ipt3Obyhm";
-  static const String kSupabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzY1MTUyMDAwLCJleHAiOjE5MjI5MTg0MDB9.bIZaH2Ew7tpxt7B0QlN_aakowr6EjoK7c0GYr3_Jz3k';
-  static const String kAzuraCastApiKey = 'c86cfe8a7b3ac365:02c4a4e17a736ba02198aa2864b1e686';
-  static const String kResendApiKey = 're_CRVSaiDi_MdhNk2jbKi3NyGp8f69PvxZr';
-  static const String kRegisterEmailTokenSecret = 'xdVtxoqmpT396jEB';
-  static const String kMapboxTileUrl = '<mapbox-tile-url>';
-}
